da645c983e602bf8c6fa012e0cd1a5f8b3a65dbb
ozgekaragoz <ozgekaragozz00@gmail.com>
2025-01-29 11:33:19 +0300

    app.py dosyasında değişiklik yapıldı.

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
 from streamlit as st
-from extract_text_from_pdf import extract_text_from_pdf
+from parser import extract_text_from_pdf
 from cleaner import clean_text
 from embedding import embed_text
 from indexing import build_faiss_index
 from query_handler import handle_query
 
 @st.cache_data
-def initialize_system(pdf_path):
-    text  = extract_text_from_pdf(pdf_path)
+def initialize_system(pdfs\clean_code.pdf):
+    text  = extract_text_from_pdf(pdfs\clean_code.pdf)
     cleaned_text = clean_text(text)
     segments = segment_text(cleaned_text)
-    embeddings = embed_text(segments)
+
+    image_folder = "images"
+    image_files = extract_images_from_pdf(pdfs\clean_code.pdf, image_folder)
+    image_texts = [extract_text_from_image(os.path.join(image_folder, img)) for img in image_files]
+
+    all_texts = segments + image_texts
+    embeddings = embed_text(all_texts)
     index = build_faiss_index(embeddings)
-    return index, segments
+    return index, all_texts
 
 pdf_path = "./pdfs/clean_code.pdf"
-index, segments = initialize_system(pdf_path)
+index, segments = initialize_system(pdfs\clean_code.pdf)
 
 st.title("Clean Code Chatbot")
 
