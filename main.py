316634e18fd33e9bd8bcc17a2a4fb3103f65e838
ozgekaragozz <ozge.karagoz@nttdata.com>
2025-01-31 15:31:35 +0300

    Main dosyası da class yapısına uygun hale getirildi.

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
-from extract_text_from_pdf import extract_text_from_pdf
+from parser import PDFProcessor
 from cleaner import clean_text, segment_text
-from embedding import embed_text
+from embedding import embed_with_multiple_models
 from indexing import build_faiss_index
 from query_handler import handle_query
 
 pdf_path = "./pdfs/clean_code.pdf"
+pdf_processor = PDFProcessor(pdf_path)
+
 print("PDF is being processed.")
-text = extract_text_from_pdf(pdf_path)
+text = pdf_processor.extract_text()
+
+print("Extracting images from PDF...")
+image_output_folder = "./extracted_images"
+pdf_processor.extract_images(image_output_folder)
 
 print("Text is cleaning and segmenting.")
 cleaned_text = clean_text(text)
-segments  =segment_text(cleaned_text)
+segments = segment_text(cleaned_text)
 
 print("Embedding is creating.")
-embedding = embed_text(segments)
+embeddings = embed_with_multiple_models(segments)
 
 print("Index of FAISS is creating.")
 index = build_faiss_index(embeddings)
 
 while True:
-    query = input("Please enter the your question. If you want to exit, press q. ").strip()
+    query = input("Please enter your question. If you want to exit, press 'q': ").strip()
     if query.lower() == 'q':
-        print("Exit is in progress.")
+        print("Exiting...")
         break
-    
+
     response = handle_query(query, index, segments)
-    print("\nAnswer")
+    print("\nAnswer:")
     print(response)
